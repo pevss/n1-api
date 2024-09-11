2b06da91520bf6a7b3cf8002d831bd63ba1578de
pevss <pedrov3245@gmail.com>
2024-09-11 11:33:49 -0300

    Add JSDocs to helpers file's functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,11 @@
 const axios = require("axios");
 const { API_KEY } = require("./config");
 
+/**
+ * @param {string} URL API url
+ * @returns {object} The response object returned from the request
+ * @throws Will throw an error if the request fails
+ */
 const getJSON = async function (url) {
 	try {
 		const data = await axios.get(url, {
@@ -15,6 +20,12 @@ const getJSON = async function (url) {
 	}
 };
 
+/**
+ * @param {object[]} Collection Array of objects, where every object has an "id" property
+ * @param {number} ID The ID of the item to be deleted
+ * @returns {object} A copy of the deleted object
+ * @throws Will throw an error if the item could not be found inside the collection
+ */
 const deleteFrom = function (collection, id) {
 	try {
 		const idToBeDeleted = collection.findIndex((item) => item.id === +id);
